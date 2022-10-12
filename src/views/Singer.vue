e526fe3740940c2879f2d5c47e965cda357abdaa
kitety <aizaizuori@foxmail.com>
2022-10-12 23:40:09 +0800

    feat: singer list animate

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -14,10 +14,19 @@
       </ul>
       <ul class="list-keys">
         <li v-for="(value,index) in alphabet" :key="index" :class="{active:activeIndex===index}"
-            @click.stop="clickKey(index)">
+            :data-index="index"
+            @click.stop="clickKey(index)"
+            @touchstart.stop.prevent="touchStart"
+            @touchmove.stop.prevent="touchMove"
+        >
           {{ value }}
         </li>
+        <!--        <li v-for="(value,index) in alphabet" :key="index" :class="{active:activeIndex===index}"-->
+        <!--            @click.stop="clickKey(index)">-->
+        <!--          {{ value }}-->
+        <!--        </li>-->
       </ul>
+      <div v-show="fixedTitle" class="fixed-title">{{ fixedTitle }}</div>
     </ScrollView>
   </div>
 </template>
@@ -36,7 +45,11 @@ export default {
       alphabet: [],
       singers: [],
       groupsTop: [],
-      activeIndex: 0
+      activeIndex: 0,
+      beginOffsetY: 0,
+      moveOffsetY: 0,
+      isClick: false,
+      scrollY: 0
     }
   },
   watch: {
@@ -58,12 +71,65 @@ export default {
     this.alphabet = alphabet
     this.singers = singers
   },
+  computed: {
+    fixedTitle () {
+      if (this.scrollY > 0) {
+        return ''
+      } else {
+        return this.alphabet[this.activeIndex]
+      }
+    }
+  },
+  mounted () {
+    const scrollView = this.$refs.scrollView
+    scrollView.scrolling(y => {
+      if (this.isClick || isNaN(y)) {
+        return
+      }
+      this.scrollY = y
+      if (y >= 0) {
+        this.activeIndex = 0
+        return
+      }
+      //  处理中间
+      const value = Math.abs(y)
+      const index = this.groupsTop.findIndex((item, index) => {
+        return item <= value && value <= this.groupsTop[index + 1]
+      })
+      if (index >= 0) {
+        this.activeIndex = index
+      } else {
+        this.activeIndex = this.groupsTop.length - 1
+      }
+    })
+  },
   methods: {
-    clickKey (index) {
+    _clickKey (index) {
       console.log(index)
       this.activeIndex = index
       const top = this.groupsTop[index]
-      this.$refs.scrollView.scrollTo(0, -top, 300)
+      this.isClick = true
+      const time = 500
+      this.$refs.scrollView.scrollTo(0, -top, time)
+      setTimeout(() => {
+        this.isClick = false
+      }, time + 100)
+    },
+    touchStart (e) {
+      const { index } = e.target.dataset
+      this._clickKey(Number(index))
+      this.beginOffsetY = e.touches[0].pageY
+    },
+    touchMove (e) {
+      this.moveOffsetY = e.touches[0].pageY
+      const offsetY = (this.moveOffsetY - this.beginOffsetY) / e.target.offsetHeight
+      let index = parseInt(e.target.dataset.index) + Math.floor(offsetY)
+      index = Math.min(index, this.alphabet.length - 1)
+      index = Math.max(index, 0)
+
+      if (index !== this.activeIndex) {
+        this._clickKey(index)
+      }
     }
   }
 }
@@ -107,7 +173,7 @@ export default {
         p {
           @include font_size($font_medium);
           @include font_color();
-          margin-left: 120px;
+          margin-left: 30px;
         }
       }
     }
@@ -115,7 +181,7 @@ export default {
 
   .list-keys {
     position: fixed;
-    top: 60%;
+    top: 55%;
     transform: translateY(-50%);
     right: 10px;
 
@@ -123,11 +189,24 @@ export default {
       @include font_color();
       @include font_size($font_medium_s) ;
       padding: 3px 0;
+      text-align: center;
 
       &.active {
         text-shadow: 0 0 10px #000;
       }
     }
+  }
+
+  .fixed-title {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    @include bg_color();
+    @include font_size($font_medium);
+    color: #fff;
+    padding: 10px 20px;
+    box-sizing: border-box;
   }
 }
 
